e8313e6d6a7e0b586d068034e2217d25b79fa5c9
sucad hassan <sucad1@hotmail.co.uk>
2023-02-26 19:10:15 +0000

    updated the challange speed run

--- starter/README.md
+++ starter/README.md
@@ -1,5 +1,39 @@
 # undefined
   ![Github license](http://img.shields.io/badge/license-test-blue.svg)
 
+  ### Description
+
+  test
+
+  ### Table of Contents
+  
+  *[Installation](#installation)
+
+  *[Usage](#usage)
+
+  *[License](#license)
+
+
+  *[Contributing](#contributing)
+
+  *[Test](#test)
+
+  *[Questions](#questions)
+
+  ## Installation
+
+  To instrall necessary dependencies, run the following command:
+
+  ```
+  test
+  ```
+
+  ## Usage 
+  test
+
+
+  
+
+
 
 
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,14 +1,79 @@
 // function to generate markdown for README
-function renderLicenseBadge(license) {
+const renderLicenseBadge = (license) => {
   if(license !== "None") {
     return `![Github license](http://img.shields.io/badge/license-${license}-blue.svg)`
   }
   return ``
 }
 
+const renderLicenseLink = (license) => {
+  if (license !== "None") {
+    return `*[License](#license)\n`
+  }
+  return``;
+
+}
+
 function generateMarkdown(data) {
+  console.log(data);
   return `# ${data.title}
   ${renderLicenseBadge(data.license)}
+
+  ### Description
+
+  ${data.description}
+
+  ### Table of Contents
+  
+  *[Installation](#installation)
+
+  *[Usage](#usage)
+
+  ${renderLicenseLink(data.license)}
+
+  *[Contributing](#contributing)
+
+  *[Test](#test)
+
+  *[Questions](#questions)
+
+  ## Installation
+
+  To instrall necessary dependencies, run the following command:
+
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+
+  ## Usage 
+  ${data.usage}
+
+  ## License
+
+  This project is licensed under the ${data.license} license
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  To run tests, run the following command:
+
+  \`\`\`
+  ${data.test}
+  \`\`\`
+
+  ### Questions
+
+  If you have question about the respo, contact me directly at ${data.email}.
+
+  You can find more of my work at [${data.github}] (https://github.com/${data.github}).
+
+
+
+  
+
 
 
 
